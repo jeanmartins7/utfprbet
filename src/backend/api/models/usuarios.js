7f0a4b48afab0df2f8c55d65a57bbbdb4952bf86
Jean Martins <jeanmarcel8@gmail.com>
2023-06-12 02:00:23 -0300

    criando tabelas

---
+++ src/backend/api/models/usuarios.js
@@ -0,0 +1,12 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const usuarios = sequelize.define('usuarios', {
+    nome: DataTypes.STRING,
+    email: DataTypes.STRING,
+    senha: DataTypes.STRING
+  }, {});
+  usuarios.associate = function(models) {
+    // associations can be defined here
+  };
+  return usuarios;
+};
